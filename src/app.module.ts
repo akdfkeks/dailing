29e5a3695415eb9baa6a3fb294dc62d7872126cc
akdfkeks <akdfkeks@gmail.com>
2022-11-27 13:24:56 +0900

    add: Auth 및 Schedule 리소스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ScheduleModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
